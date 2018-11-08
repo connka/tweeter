15a5f9c22afe3d223cc88c1ae8de64dab37dad78
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-08 03:18:29 +0000

    character counter added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
-$$ ("document").ready(() => {
+$ ("document").ready(() => {
 
+$("#newTweet").on('keyup', function() {
+  let charAmount = this.value.length;
+  const maxChar = 140;
+  let countdown = (maxChar - charAmount);
+  $(this).parent().children("span").html(maxChar - (($(this).val()).length));
+  if (countdown < 0) {
+    document.getElementById("counter").style.color = "#ff0000";
+  } else {
+    document.getElementById("counter").style.color = "#000000";
+  }
+})
 })
